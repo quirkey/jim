03b475aaceb1a1993593fde93039aa58e5d9febe
Aaron Quint <aaron@quirkey.com>
2010-01-30 14:42:14 -0500

    Initial dump of CLI ideas

---
+++ lib/jim/cli.rb
@@ -0,0 +1,50 @@
+module Jim
+  class CLI
+    
+    attr_accessor :jimfile
+    
+    def intitialize(args)
+      @args = parse_options(args)
+      
+    end
+    
+    def init
+      
+    end
+    
+    def install
+      
+    end
+    
+    def bundle
+      
+    end
+    
+    def vendor
+      
+    end
+    
+    private
+    def parse_options(runtime_args)
+      OptionParser.new("", 24, '  ') do |opts|
+        opts.banner = "Usage: jim [options] [args]"
+
+        opts.separator ""
+        opts.separator "jim options:"
+
+        opts.on("-j", "--jimfile path/to/jimfile", "load specific jimfile at path (default ./jimfile)") { |j|
+          self.jimfile = Pathname.new(j)
+        }
+        opts.on_tail("-h", "--help", "Show this message") do
+          puts opts
+          exit
+        end
+
+      end.parse! runtime_args
+    rescue OptionParser::MissingArgument => e
+      logger.warn "#{e}, run -h for options"
+      exit
+    end
+    
+  end
+end
---
+++ test/test_jim_cli.rb
@@ -0,0 +1,15 @@
+require 'helper'
+
+class TestJimCLI < Test::Unit::TestCase
+
+  context "Jim::CLI" do
+    setup do
+      
+    end
+    
+    
+    
+    
+  end
+
+end
