3e01e4374e7e9fb84bc1baf8af32e361e8c4b341
Aaron Quint <aaron@quirkey.com>
2010-11-13 15:22:08 -0600

    Removing logger refs and using say instead

--- lib/jim/cli.rb
+++ lib/jim/cli.rb
@@ -1,6 +1,4 @@
 require 'thor'
-require 'thor/actions'
-
 
 module Jim
 
@@ -43,7 +41,6 @@ module Jim
         say "jim #{Jim::VERSION}", :red
         exit
       end
-      @output = ""
       # set the default jimhome
       self.jimhome = Pathname.new(options[:jimhome] || ENV['JIMHOME'] || '~/.jim').expand_path
       # parse the options
@@ -52,21 +49,6 @@ module Jim
       self.debug = options[:debug]
       logger.level = Logger::DEBUG if debug
     end
-
-    # def commands
-    #   logger.info "Usage: jim [options] [command] [args]\n"
-    #   logger.info "Commands:"
-    #   logger.info template('commands')
-    # end
-    #
-    # # list the possible commands without detailed descriptions
-    # def cheat
-    #   logger.info "Usage: jim [options] [command] [args]\n"
-    #   logger.info "Commands:"
-    #   logger.info [*template('commands')].grep(/^\w/).join
-    #   logger.info "run commands for details"
-    # end
-    # alias :help :cheat
 
     desc 'init [APPDIR]', 'Create an example Jimfile at path or the current directory if path is omitted'
     def init(dir = nil)
@@ -86,13 +68,13 @@ module Jim
       "Install the file(s) at url into the JIMHOME directory."
 
     long_desc <<-EOT
-Install the file(s) at url into the JIMHOME directory. URL can be any path
-or url that Downlow understands. This means:
+      Install the file(s) at url into the JIMHOME directory. URL can be any path
+      or url that Downlow understands. This means:
 
-  jim install http://code.jquery.com/jquery-1.4.1.js
-  jim install ../sammy/
-  jim install gh://quirkey/sammy
-  jim install git://github.com/jquery/jquery.git
+        jim install http://code.jquery.com/jquery-1.4.1.js\n
+        jim install ../sammy/\n
+        jim install gh://quirkey/sammy\n
+        jim install git://github.com/jquery/jquery.git\n
     EOT
     def install(url, name = false, version = false)
       Jim::Installer.new(url, jimhome, :force => force, :name => name, :version => version).install
@@ -105,6 +87,7 @@ or url that Downlow understands. This means:
       If the bundled_path is not set, jim will try to use the bundled_path set in
       the Jimfile. If no bundled_path is found - it will output the entire bundle
       to STDOUT.
+
       If no Jimfile is set in the options, assumes ./Jimfile.
     EOT
     def bundle(to = nil)
@@ -118,9 +101,11 @@ or url that Downlow understands. This means:
     long_desc <<-EOT
       Bundle all the files listed in a Jimfile, run through the google closure
       compiler and save them to [compressed_path].
+
       If the compressed_path is not set, jim will try to use the compressed_path
       set in the Jimfile. If no compressed_path is found - it will output the
       entire compressed bundle to STDOUT.
+
       If no Jimfile is set in the options, assumes ./Jimfile.
     EOT
     def compress(to = nil)
@@ -136,43 +121,43 @@ or url that Downlow understands. This means:
 
     desc "list [SEARCH]", "List all the installed packages and their versions, optionally limiting by [SEARCH]"
     def list(search = nil)
-      logger.info "Getting list of installed files in\n#{installed_index.directories.join(':')}"
-      logger.info "Searching for '#{search}'" if search
+      say "Getting list of installed files in\n#{installed_index.directories.join(':')}"
+      say "Searching for '#{search}'" if search
       list = installed_index.list(search)
-      logger.info "Installed:"
+      say "Installed:"
       print_version_list(list)
     end
     map "installed" => "list"
 
     desc "available [SEARCH]" ,"List all available projects and versions including those in the local path, or paths specified in a Jimfile"
     def available(search = nil)
-      logger.info "Getting list of all available files in\n#{index.directories.join("\n")}"
-      logger.info "Searching for '#{search}'" if search
+      say "Getting list of all available files in\n#{index.directories.join("\n")}"
+      say "Searching for '#{search}'" if search
       list = index.list(search)
-      logger.info "Available:"
+      say "Available:"
       print_version_list(list)
     end
 
     desc "remove <NAME> [VERSION]", "Iterate through the install files and prompt for the removal of those matching the supplied NAME and VERSION"
     def remove(name, version = nil)
-      logger.info "Looking for files matching #{name} #{version}"
+      say "Looking for files matching #{name} #{version}"
       files = installed_index.find_all(name, version)
       if files.length > 0
-        logger.info "Found #{files.length} matching files"
+        say "Found #{files.length} matching files"
         removed = 0
         files.each do |filename|
-          response = Readline.readline("Remove #{filename}? (y/n)\n")
-          if response.strip =~ /y/i
-            logger.info "Removing #{filename}"
+          do_remove = yes?("Remove #{filename}?", :red)
+          if do_remove
+            say "Removing #{filename}"
             filename.delete
             removed += 1
           else
-            logger.info "Skipping #{filename}"
+            say "Skipping #{filename}", :yellow
           end
         end
-        logger.info "Removed #{removed} files."
+        say "Removed #{removed} files."
       else
-        logger.info "No installed files matched."
+        say "No installed files matched."
       end
     end
     map "uninstall" => "remove"
@@ -180,16 +165,16 @@ or url that Downlow understands. This means:
     desc "resolve", "Resolve all the paths listed in a Jimfile and print them to STDOUT. If no Jimfile is set in the options, assumes ./Jimfile."
     def resolve
       resolved = bundler.resolve!
-      logger.info "Files:"
+      say "Files:"
       resolved.each do |r|
-        logger.info r.join(" | ")
+        say r.join(" | ")
       end
       resolved
     end
 
     desc "pack [DIR]", "Runs in order, vendor, bundle, compress. This command simplifies the common workflow of vendoring and re-bundling before committing or deploying changes to a project"
     def pack(dir = nil)
-      logger.info "packing the Jimfile for this project"
+      say "packing the Jimfile for this project"
       invoke :vendor, [dir]
       invoke :bundle
       invoke :compress
