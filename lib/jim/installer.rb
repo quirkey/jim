6674297daeff4574d993ac3dec9ccbf458c0a849
Aaron Quint <aaron@quirkey.com>
2010-11-21 16:09:05 -0800

    Cleanup

--- lib/jim/installer.rb
+++ lib/jim/installer.rb
@@ -1,11 +1,11 @@
 module Jim
-  # Installer is the workhorse of Jim. It handles taking an install path 
-  # (a url, a local path, anything that Downlow.get can handle), staging it 
-  # into a temporary directory and extracting the file(s) into a path for the 
-  # specific name and version of the lib. names and versions are determined 
-  # automatically or can be passed in as options. 
+  # Installer is the workhorse of Jim. It handles taking an install path
+  # (a url, a local path, anything that Downlow.get can handle), staging it
+  # into a temporary directory and extracting the file(s) into a path for the
+  # specific name and version of the lib. names and versions are determined
+  # automatically or can be passed in as options.
   class Installer
-    
+
     IGNORE_DIRS = %w{
       vendor
       external
@@ -19,19 +19,19 @@ module Jim
       \_([^\/]+)
       \.([^\/]+)
     }
-    
+
     # get the tmp_root where files are staged
     def self.tmp_root
       @tmp_root ||= Pathname.new('/tmp/jim')
     end
-    
+
     # set the tmp_root where files are staged. Default: '/tmp/jim'
     def self.tmp_root=(new_tmp_root)
       @tmp_root = Pathname.new(new_tmp_root)
     end
-        
+
     attr_reader :fetch_path, :install_path, :options, :fetched_path, :name, :version, :package_json
-    
+
     # create an installer. fetch_path is anything that Downlow can understand.
     # install path is the final directory
     def initialize(fetch_path, install_path, options = {})
@@ -40,12 +40,12 @@ module Jim
       @options      = options
     end
 
-    # fetch the file at fetch_path with and stage into a tmp directory. 
+    # fetch the file at fetch_path with and stage into a tmp directory.
     # returns the staged directory of fetched file(s).
     def fetch
-      logger.info "fetching #{fetch_path}"
+      logger.info "Fetching #{fetch_path}"
       @fetched_path = Downlow.get(fetch_path, tmp_path, :tmp_dir => tmp_root)
-      logger.debug "fetched #{@fetched_path}"
+      logger.debug "Fetched #{@fetched_path}"
       @fetched_path
     end
 
@@ -55,77 +55,77 @@ module Jim
     # determined for. It also installs a package.json file along side the JS file
     # that contains meta data including the name and version, also merging with the
     # original package.json if found.
-    # 
+    #
     # If options[:shallow] == true it will just copy the single file without any leading
     # directories or a package.json. 'shallow' installation is used for Bundle#vendor
     def install
       fetch
       parse_package_json
       determine_name_and_version
-      
+
       if !name || name.to_s =~ /^\s*$/ # blank
-        raise(Jim::InstallError, "could not determine name for #{@fetched_path}")
+        raise(Jim::InstallError, "Could not determine name for #{@fetched_path}")
       end
-      
-      logger.info "installing #{name} #{version}"
+
+      logger.info "Installing #{name} #{version}"
       logger.debug "fetched_path #{@fetched_path}"
-      
+
       if options[:shallow]
         shallow_filename = [name, (version == "0" ? nil : version)].compact.join('-')
         final_path = install_path + "#{shallow_filename}#{fetched_path.extname}"
       else
         final_path = install_path + 'lib' + "#{name}-#{version}" + "#{name}.js"
       end
-      
+
       if @fetched_path.directory?
         # install every js file
         installed_paths = []
         sub_options = options.merge({
-          :name => nil, 
+          :name => nil,
           :version => nil,
-          :parent_version => version, 
+          :parent_version => version,
           :package_json => package_json.merge("name" => nil)
         })
         Jim.each_path_in_directories([@fetched_path], '.js', IGNORE_DIRS) do |subfile|
-          logger.info "found file #{subfile}"
+          logger.info "Found file #{subfile}"
           installed_paths << Jim::Installer.new(subfile, install_path, sub_options).install
         end
-        logger.info "extracted to #{install_path}, #{installed_paths.length} file(s)"
+        logger.info "Extracted to #{install_path}, #{installed_paths.length} file(s)"
         return installed_paths
       end
-      
-      logger.debug "installing to #{final_path}"
-      if final_path.exist? 
+
+      logger.debug "Installing to #{final_path}"
+      if final_path.exist?
         logger.debug "#{final_path} already exists"
         if options[:force]
           FileUtils.rm_rf(final_path)
         elsif Digest::MD5.hexdigest(File.read(final_path)) == Digest::MD5.hexdigest(File.read(@fetched_path))
-          logger.info "duplicate file, skipping"
+          logger.info "Duplicate file, skipping"
           return final_path
         else
           raise(Jim::FileExists.new(final_path))
         end
       end
-      
+
       Downlow.extract(@fetched_path, :destination => final_path, :tmp_dir => tmp_root)
       # install json
       install_package_json(final_path.dirname + 'package.json') if !options[:shallow]
       installed = final_path.directory? ? Dir.glob(final_path + '**/*').length : 1
-      logger.info "extracted to #{final_path}, #{installed} file(s)"
+      logger.info "Extracted to #{final_path}, #{installed} file(s)"
       final_path
     ensure
       FileUtils.rm_rf(@fetched_path) if @fetched_path && @fetched_path.exist?
       final_path
     end
-    
-    # determine the name and version of the @fetched_path. Tries a number of 
+
+    # determine the name and version of the @fetched_path. Tries a number of
     # strategies in order until both name and version are found:
     #
     # * from options (options[:name] ...)
     # * from comments (// name: )
     # * from a package.json ({"name": })
     # * from the filename (name-1.0.js)
-    # 
+    #
     # If no version can be found, version is set as "0"
     def determine_name_and_version
       (name && version) ||
@@ -138,7 +138,7 @@ module Jim
 
     private
     def tmp_root
-      @tmp_root ||= make_tmp_root 
+      @tmp_root ||= make_tmp_root
     end
 
     def tmp_dir
@@ -152,17 +152,17 @@ module Jim
     def logger
       Jim.logger
     end
-    
+
     def make_tmp_root
       self.class.tmp_root + (Time.now.to_i + rand(10000)).to_s
     end
-    
+
     def make_tmp_dir
       dir = tmp_root + fetch_path.stem
       dir.mkpath
       dir
     end
-    
+
     def parse_package_json
       @package_json = @options[:package_json] || {}
       package_json_path = if fetched_path.directory?
@@ -180,7 +180,7 @@ module Jim
 
     def install_package_json(to_path, options = {})
       hash = @package_json.merge({
-        "name" =>  name, 
+        "name" =>  name,
         "version" => version,
         "install" => {
           "at" => Time.now.httpdate,
