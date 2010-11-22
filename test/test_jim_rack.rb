076cb6a138abedd5c37eb31eb6d856e2702ba0cd
Aaron Quint <aaron@quirkey.com>
2010-11-21 18:40:15 -0800

    Dont need any of this setup

--- test/test_jim_rack.rb
+++ test/test_jim_rack.rb
@@ -15,19 +15,11 @@ class TestJimRack < Test::Unit::TestCase
       use Jim::Rack, :bundle_uri => @bundle_uri,
                      :jimfile => jimfile,
                      :jimhome => File.join(File.dirname(__FILE__), 'tmp')
-      run lambda { |env| [200, {'Content-Type' => 'text/plain'}, 'okay' ] }
+      run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['okay']] }
     }
   end
 
   context "Jim::Rack" do
-    setup do
-      # clear the tmp dir
-      FileUtils.rm_rf(tmp_path) if File.directory?(tmp_path)
-      root = File.dirname(__FILE__)
-      Jim::Installer.new(fixture_path('jquery-1.4.1.js'), File.join(root, 'tmp', 'lib')).install
-      Jim::Installer.new(fixture_path('infoincomments.js'), File.join(root, 'tmp', 'lib')).install
-      Jim::Installer.new(fixture_path('localfile.js'), File.join(root, 'tmp', 'lib')).install
-    end
 
     should "get individual bundle" do
       Jim::Bundler.any_instance.expects(:bundle!).with('default').once.returns('jQuery')
